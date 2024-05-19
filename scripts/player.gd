388411ac739d48676fd7eb39b93dc18e29837c7a
Semmel92 <2210859038@fh-burgenland.at>
2024-05-19 18:38:33 +0200

    added fourth_szene Camera

--- scripts/player.gd
+++ scripts/player.gd
@@ -105,7 +105,7 @@ func _on_player_hitbox_body_exited(body):
 
 func enemy_attack():
 	if enemy_inattack_range and enemy_attack_cooldown == true:
-		health = health - 80
+		health = health - 20
 		enemy_attack_cooldown = false
 		$attack_cooldown.start()
 		print(health)
