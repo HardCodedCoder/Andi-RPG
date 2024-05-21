ca8c5d31880a5ba4ad447da6506927411e2eb063
Semmel92 <2210859038@fh-burgenland.at>
2024-05-21 12:55:15 +0200

    add more enemys

--- gameover/game_over.gd
+++ gameover/game_over.gd
@@ -6,7 +6,7 @@ extends Control
 @onready var again_button = $MarginContainer/HBoxContainer/VBoxContainer/AgainButton as Button
 @onready var back_button = $MarginContainer/HBoxContainer/VBoxContainer/BackButton as Button
 
-@onready var restart_level = preload("res://scenes/start_scene.tscn") as PackedScene
+@onready var restart_level = load("res://scenes/start_scene.tscn") as PackedScene
 @onready var return_to_menu = load("res://mainmenu/main_menu.tscn") as PackedScene
 
 func _ready():
